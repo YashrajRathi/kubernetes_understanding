7b02bd1356d3dfa4dd8ccad82f74250b0286066a
YashrajRathi <yashraj11rathi@gmail.com>
2025-07-06 18:04:24 +0530

    Automation script

---
+++ download_needed_scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -O https://raw.githubusercontent.com/YashrajRathi/kubernetes_understanding/refs/heads/main/complete_setup.sh
+curl -O https://raw.githubusercontent.com/YashrajRathi/kubernetes_understanding/refs/heads/main/set_complete_kubernetes_on_aws_ec2.sh
+
+chmod 777 complete_setup.sh
+chmod 777 set_complete_kubernetes_on_aws_ec2.sh
